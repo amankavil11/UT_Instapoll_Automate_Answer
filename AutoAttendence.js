80060724583935ac2a621928433825018219063d
Abe Mathew Mankavil <abe.m.mankavil@gmail.com>
2023-03-23 10:21:06 -0500

    Create AutoAttendence.js

---
+++ AutoAttendence.js
@@ -0,0 +1,23 @@
+/*
+Instructions:
+	1. Login to your canvas, then redirect the webpage to online class video stream.
+	2. Copy the snippet below
+	3. Then just paste it inside a Chrome or Firefox developer console to run
+	4. If you see the console print ("Find Button") then the script is working
+This script should automatically select a poll answer whenever there is a new one popping up
+*/
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+async function clickButton()
+{
+    let button = document.getElementsByClassName("btn btn-primary");
+    console.log("Find Button");
+    if(button.length > 0)
+    {
+        button[0].click();
+        console.log("Button clicked")
+        await sleep(6000)
+    }
+    setTimeout(function(){ clickButton();}, 500);
+}
+clickButton();
